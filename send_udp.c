dd6a46d3e34f78e67cb2f7f090f21a23c56c6aca
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-10-24 09:52:00 -0600

    creates and connects socket and reads from standard input and write to the file descriptor connected to the server (still needs testing with file #2)

--- send_udp.c
+++ send_udp.c
@@ -2,19 +2,19 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <errno.h>
-
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <string.h>
+/*function declarations*/
+ssize_t better_write(int fd, const char *buf, size_t count);
+int reading_and_sending(int socket_fd, char *buf);
 
 #define SIZE 460
-
 int main(int argc, char ** argv){
     char *message, *server_name, *port_name;
     int socket_fd, gai_code;
     char buf[SIZE];
-    ssize_t read_res;
-    ssize_t bytes_sent;
     if (argc < 3){
         message = "Not enough arugments, expected: <server-name> <port>\n";
         better_write(1, message, sizeof(message));
@@ -22,7 +22,7 @@ int main(int argc, char ** argv){
     }
     server_name = argv[1];
     port_name = argv[2];
-    struct addrinfo hints, *result;
+    struct addrinfo hints, *result, *current;
     
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -30,29 +30,47 @@ int main(int argc, char ** argv){
     hints.ai_protocol = 0;
     hints.ai_flags = 0;
     gai_code = getaddrinfo(server_name, port_name, &hints, &result);
-    
-    if (getaddrinfo(server_name, port_name, &hints, &result)){
-        fprintf(stderr, "Error wiht getaddrinfo: %s\n", strerror(errno));
-        return 1;
+    if (gai_code != 0){
+      fprintf(stderr, "Error with getaddrinfo: %s\n", strerror(errno));
+      return 1;
     }
-    
     socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (socket_fd < 0){
-        fprintf(stderr, "Error: could not create socket\n", strerror(errno));
+        fprintf(stderr, "Error: could not create socket\n");
         return 1;
     }
     /* we need to read from standard input and send the data to the server
-     * instead of writing to standard input, we send to the socket_fd
-     * do we need to connect after creating the socket?
+        * instead of writing to standard input, we send to the socket_fd
+        * do we need to connect after creating the socket?
     */
+    for (current = result; current != NULL; current = current->ai_next){
+        if (connect(socket_fd, current->ai_addr, current->ai_addrlen) == 0){
+        break;
+        }
+    }
 
+    
+    if (current==NULL){
+        fprintf(stderr, "Error: could not connect.");
+        freeaddrinfo(result);
+        return 1;
+    }
+    if (reading_and_sending(socket_fd, buf) > 0){
+        fprintf(stderr, "Error: could not read.");
+        return 1;
+    }
+    freeaddrinfo(result);
+    return 0;
+}
 
-
+int reading_and_sending(int socket_fd, char *buf){
+    ssize_t read_res;
+    ssize_t bytes_sent;
     while ((read_res = read(0, buf, sizeof(buf)) > 0)){
         if (read_res < ((ssize_t) 0)) {
         /* There has been an error on read() */
-        fprintf(stderr, "Error using read: %s\n", strerror(errno));
-        return 1;
+            fprintf(stderr, "Error using read: %s\n", strerror(errno));
+            return 1;
         }
 
         bytes_sent = send(socket_fd, buf, sizeof(buf), 0);
@@ -61,9 +79,7 @@ int main(int argc, char ** argv){
             fprintf(stderr, "Error using send: %s\n",strerror(errno));
             return 1;
         }
-
     }
-
     bytes_sent = send(socket_fd, "", 0, 0);
     if ( bytes_sent < 0) {
         /* There has been an error on send() */
@@ -72,44 +88,32 @@ int main(int argc, char ** argv){
         return 1;
     }
     close(socket_fd);
-    freeaddrinfo(result);
-
-    
-
-
-
-
-
-
-
     return 0;
 }
-
 ssize_t better_write(int fd, const char *buf, size_t count) {
-  size_t already_written, to_be_written, written_this_time, max_count;
-  ssize_t res_write;
+    size_t already_written, to_be_written, written_this_time, max_count;
+    ssize_t res_write;
 
-  if (count == ((size_t) 0)) return (ssize_t) count;
-  
-  already_written = (size_t) 0;
-  to_be_written = count;
-  while (to_be_written > ((size_t) 0)) {
-    max_count = to_be_written;
-    if (max_count > ((size_t) 8192)) {
-      max_count = (size_t) 8192;
+    if (count == ((size_t) 0)) return (ssize_t) count;
+    already_written = (size_t) 0;
+    to_be_written = count;
+    while (to_be_written > ((size_t) 0)) {
+        max_count = to_be_written;
+        if (max_count > ((size_t) 8192)) {
+            max_count = (size_t) 8192;
+        }
+        res_write = write(fd, &(((const char *) buf)[already_written]), max_count);
+        if (res_write < ((size_t) 0)) {
+            /* Error */
+            return res_write;
+        }
+        if (res_write == ((ssize_t) 0)) {
+        /* Nothing written, stop trying */
+            
+        }
+        written_this_time = (size_t) res_write;
+        already_written += written_this_time;
+        to_be_written -= written_this_time;
     }
-    res_write = write(fd, &(((const char *) buf)[already_written]), max_count);
-    if (res_write < ((size_t) 0)) {
-      /* Error */
-      return res_write;
-    }
-    if (res_write == ((ssize_t) 0)) {
-      /* Nothing written, stop trying */
-      return (ssize_t) already_written;
-    }
-    written_this_time = (size_t) res_write;
-    already_written += written_this_time;
-    to_be_written -= written_this_time;
-  }
-  return (ssize_t) already_written;
+    return (ssize_t) already_written;
 }
